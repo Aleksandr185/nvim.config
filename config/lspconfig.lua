06287509b475df01485c433d4560d5c87548a36e
aleksandr@office.win11 <burlakin.alexandr@yandex.ru>
2024-03-24 20:39:07 +0300

    Added CMake LSP config

--- config/lspconfig.lua
+++ config/lspconfig.lua
@@ -3,6 +3,7 @@ local on_attach = base.on_attach
 local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
+local util = require'lspconfig.util'
 
 lspconfig.clangd.setup {
   on_attach = function (client, bufnr)
@@ -16,4 +17,21 @@ lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"python"},
+}
+
+lspconfig.cmake.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"cmake"},
+  init_options = {
+    buildDirectory = "build"
+  },
+  root_dir = function(pattern)
+    local cwd = vim.loop.cwd()
+    local root = util.root_pattern('CMakePresets.json', 'CTestConfig.cmake', '.git', 'build', 'cmake')(pattern)
+
+    -- prefer cwd if root is a descendant
+    return util.path.is_descendant(cwd, root) and cwd or root
+  end,
+  single_file_spport = true
 }
