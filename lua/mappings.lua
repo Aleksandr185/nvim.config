47526a3532b35e802a6b97d343bb3b67e8bd44b1
Бурлакин Александр Викторович <a.burlakin@niisva.su>
2025-02-28 15:54:20 +0300

    Добавлен mapping <l>td для TodoTelescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,6 @@
 require "nvchad.mappings"
 
+
 -- add yours here
 
 local map = vim.keymap.set
@@ -9,4 +10,5 @@ map("i", "jj", "<ESC>")
 map("n", "<leader>db", "<cmd>:DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line"} )
 map("n", "<leader>dr", ":DapContinue <CR>", { desc = "Start or continue the debugger"} )
 
+map("n", "<leader>td", ":TodoTelescope <CR>", { desc = "telescope TODO list"} )
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
