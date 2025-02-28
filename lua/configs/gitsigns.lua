0f14a1a2f86ffdd22c1def90d9afa442293227ee
Бурлакин Александр Викторович <a.burlakin@niisva.su>
2025-02-28 15:59:28 +0300

    Добавлен конфиг для 'gitsigns'

---
+++ lua/configs/gitsigns.lua
@@ -0,0 +1,10 @@
+local M = require "nvchad.configs.gitsigns"
+
+M.signs.delete = { text = "󰆴" }
+M.signs.topdelete = { text = "󰠙" }
+M.signs.changedelete = { text = "󱕗" }
+M.signs.change = { text = "󱦹" }
+
+M.numhl = true
+
+return M
